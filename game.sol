58f18fe2e3588c12a5554662727e27f4d305e307
Chris Wan <free2737@gmail.com>
2018-08-03 12:36:20 +0800

    Update Code

--- game.sol
+++ game.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.0;
 /*
+Etheron
+It is just a game.
+----------
+1 Aug 2018
 iDler
 */
 
@@ -8,7 +12,8 @@ contract Game
 	struct City
 	{
 		string	city_name;
-		uint	
+		uint	city_id;
+		uint	city_status;
 	}
 
 	struct Building
@@ -17,5 +22,38 @@ contract Game
 		uint	building_num;
 		uint	building_lv;
 		uint	building_function;
+	}
+
+	struct Hero
+	{
+		string	hero_name;
+		uint	hero_id;
+		uint	hero_lv;
+		uint	hero_skill;
+	}
+
+	address public Owner;
+	mapping(address => Player) public Players;
+
+	uint public GameEnd;
+
+	function Authorize(address Player)
+	{
+		require(msg.sender == Owner);
+		require(Players[Player] == null);
+	}
+
+	//Start the Game
+	function StartGame()
+	{
+		Owner = msg.sender;
+		GameEnd = 0;
+	}
+
+	//End the Game
+	function EndGame()
+	{
+		require(msg.sender == Owner);
+		GameEnd = 1;
 	}
 }
