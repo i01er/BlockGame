6ac27d523961893258ae77d57736abaf03a56021
Chris Wan <free2737@gmail.com>
2018-08-03 11:19:39 +0800

    Update Code

--- game.sol
+++ game.sol
@@ -3,3 +3,19 @@ pragma solidity ^0.4.0;
 iDler
 */
 
+contract Game
+{
+	struct City
+	{
+		string	city_name;
+		uint	
+	}
+
+	struct Building
+	{
+		string	building_name;
+		uint	building_num;
+		uint	building_lv;
+		uint	building_function;
+	}
+}
